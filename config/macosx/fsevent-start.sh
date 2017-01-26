2180539e1a61b04ef706b6389001be1e87228387
Paolo Mainardi <paolomainardi@gmail.com>
2017-01-26 22:12:32 +0100

    Fix script

--- config/macosx/fsevent-start.sh
+++ config/macosx/fsevent-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 killall -9 fsevents_to_vm
-fsevents_to_vm start --debug --ssh-identity-file ~/.docker/machine/machines/dinghy/id_rsa --ssh-ip $(dinghy)
+fsevents_to_vm start --ssh-identity-file ~/.docker/machine/machines/dinghy/id_rsa --ssh-ip $(dinghy ip) &
