ebbffeb8b9b1b4665ff7d6b9175cb1b5420d4170
marcello.gorla <marcello.gorla@agavee.com>
2018-12-19 14:53:50 +0100

    #22 added IP forwarding command in the launchctl script too

--- config/macosx/run-machine.sh
+++ config/macosx/run-machine.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 dinghy up
+docker-machine ssh dinghy sudo iptables -P FORWARD ACCEPT
 sudo route -n add -net 172.16.0.0/12 $(docker-machine ip dinghy)
 /usr/local/bin/fsevent-start
